cad339b2d857c0e69b5abe41745552695a8de26c
Jarek <banyaai[at]gmail.com>
2012-01-08 18:18:38 +0100

    only admins can manipulate products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,21 @@
 class ProductsController < ApplicationController
-
+  before_filter :authenticate, :only => [:new, :create, :destroy, :show]
+  before_filter :admin_user, :onlu => [:new, :create, :destroy]
+  
   def new
+    @product = Product.new
     @title = "Add product"
+  end
+
+  def create
+    @product = Product.new(params[:candidate])
+    @product.amount = 0
+    if @product.save
+      flash[:success] = "Product added successfuly"
+      redirect_to products_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -12,4 +26,20 @@ class ProductsController < ApplicationController
    @title = "All products"
    @products = Product.all
   end
+
+  def destroy
+    Product.find(params[:id].destroy
+    flash[:success] = "Product destroted"
+    redirect_to products_path
+  end
+
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end 
 end
